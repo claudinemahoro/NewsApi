b4c32a936aa78b29f0a8b2006606e4152b09746b
claudinemahoro <claudinemah3@gmail.com>
2020-11-24 10:37:42 +0200

    defining the function that gets articles based on the source id

--- app/request.py
+++ app/request.py
@@ -54,3 +54,21 @@ def process_results(sources_list):
     return sources_results
 
 
+def get_articles(id):
+    '''
+    Function that gets articles based on the source id
+    '''
+    get_articles_url = articles_url.format(id,api_key)
+
+    with urllib.request.urlopen(get_articles_url) as url:
+    
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+
+        articles_results = None
+        if get_articles_response['articles']:
+            articles_results_list = get_articles_response['articles']
+            articles_results = process_articles(articles_results_list)
+        
+    return articles_results
+
