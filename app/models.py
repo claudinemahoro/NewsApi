151ea5757cc731cc1cb0ba9503fa74ab20435bb7
claudinemahoro <claudinemah3@gmail.com>
2020-11-24 09:43:47 +0200

    define sources class

--- app/models.py
+++ app/models.py
@@ -13,22 +13,3 @@ class Sources:
         self.category = category
         self.language = language
         self.country = country
-
-
-class Articles:
-    '''
-    Sources class that defines source objects
-    '''
-    def __init__(self,id,author,title,description,url,urlToImage,publishedAt,content):
-        '''
-        Function that initiates the sources class
-        '''
-        self.id = id
-       
-        self.author = author
-        self.title = title
-        self.description = description
-        self.url = url
-        self.urlToImage = urlToImage
-        self.publishedAt = publishedAt
-        self.content = content
