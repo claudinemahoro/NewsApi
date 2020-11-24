51cc38a7a7f5a8531e6ea4ca956e7c6737515ceb
claudinemahoro <claudinemah3@gmail.com>
2020-11-24 10:36:31 +0200

    defining the function that processes the sources results

--- app/request.py
+++ app/request.py
@@ -32,3 +32,25 @@ def get_sources(category):
     
     return sources_results
 
+def process_results(sources_list):
+    '''
+    Function that processes the sources results
+    '''
+    sources_results = []
+
+    for source_item in sources_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        language = source_item.get('language')
+        country = source_item.get('country')
+
+        if url:
+            source_object = Sources(id,name,description,url,category,language,country)
+            sources_results.append(source_object)
+            
+    return sources_results
+
+
